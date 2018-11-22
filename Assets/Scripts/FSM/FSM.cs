372ee05977c8563b1e01dcfac387cda3bb81de29
James <jwdeharo@gmail.com>
2018-11-23 00:06:45 +0100

    Starting the fsm manager

--- Assets/Scripts/FSM/FSM.cs
+++ Assets/Scripts/FSM/FSM.cs
@@ -2,15 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FSM : MonoBehaviour {
+public class FSM : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private IState CurrentState;
+    private readonly Dictionary<IState, List<CCondition>> Conditions;
+    private readonly Dictionary<string, IState> States;
+    private readonly string InfoPath;
+
+    // Use this for initialization
+    private void Start()
+    {
+        //Here we will read our json for the information of the states.
+        string test = "";
+        IState state_test = null;
+        bool value = false;
+        //a for will be needed here. And inside that for we will create all the conditions.
+        //first we will need to init all the states.
+        Conditions[CurrentState].Add(new CCondition(test, state_test, value, value));
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        CurrentState.UpdateState();
+    }
+
+    private void LateUpdate()
+    {
+        //We check conditions here.
+        List<CCondition> ConditionList = GetConditionList(CurrentState);
+        foreach (CCondition Condition in ConditionList)
+        {
+            if (Condition.CheckCondition())
+            {
+                CurrentState = Condition.GetToState();
+                break;
+            }
+        }
+    }
+
+    private List<CCondition> GetConditionList(IState aState)
+    {
+        List<CCondition> ReturnListCondition = new List<CCondition>();
+
+        if (Conditions.ContainsKey(aState))
+        {
+            ReturnListCondition = Conditions[aState];
+        }
+
+        return ReturnListCondition;
+    }
+
+    private void SetFSMCondition(string aName, bool aConditionValue)
+    {
+        List<CCondition> ConditionList = GetConditionList(CurrentState);
+
+        foreach (CCondition Condition in ConditionList)
+        {
+            if (Condition.GetName().Equals(aName))
+            {
+                Condition.SetValue(aConditionValue);
+                break;
+            }
+        }
+    }
 }
