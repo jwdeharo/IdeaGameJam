99524159e2e79c5f337930ba6b0afc12d845f0db
James <jwdeharo@gmail.com>
2018-11-23 00:06:34 +0100

    Condition class

---
+++ Assets/Scripts/FSM/CCondition.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCondition 
+{
+    //Name of the condition.
+    private readonly string Name;
+    private readonly IState ToState;
+    private readonly bool NeededValue;
+    private bool CurrentValue;
+
+    public CCondition(string aName, IState aToState, bool aValue, bool aCurrentValue)
+    {
+        Name = aName;
+        ToState = aToState;
+        NeededValue = aValue;
+        CurrentValue = aCurrentValue;
+    }
+
+    public bool CheckCondition()
+    {
+        return NeededValue == CurrentValue;
+    }
+
+    public string GetName()
+    {
+        return Name;
+    }
+
+    public void SetValue(bool aValue)
+    {
+        CurrentValue = aValue;
+    }
+
+    public IState GetToState()
+    {
+        return ToState;
+    }
+}
