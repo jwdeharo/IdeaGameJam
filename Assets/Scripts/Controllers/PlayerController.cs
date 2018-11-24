8508ee16f9fcee0bb1600860641c29b1a5c360b8
James <jwdeharo@gmail.com>
2018-11-24 09:49:00 +0100

    Adding the dash state to the player fsm

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -8,8 +8,10 @@ public class PlayerController : MonoBehaviour {
     private FSM MyFsmMachine;
     //Character controller that will help us to move and detect collisions.
     private CharacterController MyController;
+
     private IdleState MyIdleState;
     private MoveState MyMoveState;
+    private DashState MyDashState;
 
     public float MoveSpeed = 5.0f;
 
@@ -22,14 +24,20 @@ public class PlayerController : MonoBehaviour {
         ////We start the states here.
         MyIdleState = new IdleState();
         MyMoveState = new MoveState();
-        
+        MyDashState = new DashState();
+
+        //We define conditions to change between states here.
         CCondition IdleToMove = new CCondition("is_moving", MyMoveState, true, false);
         CCondition MoveToIdle = new CCondition("is_moving", MyIdleState, false, false);
+        CCondition IdleToDash = new CCondition("is_dashing", MyDashState, true, false);
         
         MyFsmMachine.AddState("Idle", MyIdleState);
         MyFsmMachine.AddState("Move", MyMoveState);
+        MyFsmMachine.AddState("Dash", MyDashState);
+
         MyFsmMachine.AddCondition(MyIdleState, IdleToMove);
         MyFsmMachine.AddCondition(MyMoveState, MoveToIdle);
+        MyFsmMachine.AddCondition(MyIdleState, IdleToDash);
     }
 
     // Each state will call this function and will move according its characteristics.
