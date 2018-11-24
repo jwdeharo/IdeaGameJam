25dd9f787f2220b5d594b3d43da63574bf7abbb3
James <jwdeharo@gmail.com>
2018-11-24 10:50:29 +0100

    Adding on dash state enter behaviour

--- Assets/Scripts/FSM/MainCharacter/DashState.cs
+++ Assets/Scripts/FSM/MainCharacter/DashState.cs
@@ -4,19 +4,40 @@ using UnityEngine;
 
 public class DashState : IState {
 
+    private const float DashSpeed = 10.0f;
+    private const float StartDashTime = 0.1f;
+
+    private float DashTime;
+
     private PlayerController MyPlayerController;
     private FSM MyFsm;
+    private GameObject ThePlayer;
 
     public void OnEnterState()
     {
+        Debug.Log("Dash state enter");
         //When we enter the state, we find the player's PlayerController and its fsm to send the condition has changed.
-        MyPlayerController = GameObject.Find("Player").GetComponent<PlayerController>();
-        MyFsm = GameObject.Find("Player").GetComponent<FSM>();
+
+        if (ThePlayer == null)
+        {
+            ThePlayer = GameObject.Find("Player");
+        }
+        if (MyPlayerController == null)
+        {
+            MyPlayerController = ThePlayer.GetComponent<PlayerController>();
+        }
+
+        if (MyFsm == null)
+        {
+            MyFsm = ThePlayer.GetComponent<FSM>();
+        }
+
+        MyFsm.SetFSMCondition("is_dashing", true);
+        DashTime = StartDashTime;
     }
 
     public void UpdateState()
     {
-
     }
 
     public void OnExitState()
