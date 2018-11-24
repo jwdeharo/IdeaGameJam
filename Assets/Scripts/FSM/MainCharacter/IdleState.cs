ec9491c11c1196e9a3e003e3f0d01b6146c9f258
James <jwdeharo@gmail.com>
2018-11-24 10:50:47 +0100

    Fixed a bug with the idle state.

--- Assets/Scripts/FSM/MainCharacter/IdleState.cs
+++ Assets/Scripts/FSM/MainCharacter/IdleState.cs
@@ -4,24 +4,35 @@ using UnityEngine;
 
 public class IdleState : IState
 {
+    private GameObject          ThePlayer;
     private PlayerController    MyPlayerController;
     private FSM                 MyFsm;
 
     public void OnEnterState()
     {
         //When we enter the state, we find the player's PlayerController and its fsm to send the condition has changed.
-        MyPlayerController  =   GameObject.Find("Player").GetComponent<PlayerController>();
-        MyFsm               =   GameObject.Find("Player").GetComponent<FSM>();
+        if (ThePlayer == null)
+        {
+            ThePlayer = GameObject.Find("Player");
+        }
+
+        if (MyPlayerController == null)
+        {
+            MyPlayerController = ThePlayer.GetComponent<PlayerController>();
+        }
+
+        if (MyFsm == null)
+        {
+            MyFsm = ThePlayer.GetComponent<FSM>();
+        }
+
+        MyFsm.SetFSMCondition("is_moving", false);
+        MyFsm.SetFSMCondition("is_dashing", false);
     }
 
     public void UpdateState()
     {
-        //The UpdateState does not do much yet. 
-        if (InputManager.GetJoystickMovement() != Vector3.zero)
-        {
-            //If the input is different from 0, then this means that we're moving.
-            MyFsm.SetFSMCondition("is_moving", true);
-        }
+        MyPlayerController.SetLastPosition(ThePlayer.transform.position);
     }
 
     public void OnExitState()
