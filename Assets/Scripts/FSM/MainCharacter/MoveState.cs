20d88b22e6d1f168f87c10721ef16ddebcbf7d0c
James <jwdeharo@gmail.com>
2018-11-24 13:34:51 +0100

    Refactor

--- Assets/Scripts/FSM/MainCharacter/MoveState.cs
+++ Assets/Scripts/FSM/MainCharacter/MoveState.cs
@@ -7,28 +7,41 @@ public class MoveState : IState
 
     private PlayerController MyPlayerController;
     private FSM MyFsm;
+    private GameObject ThePlayer;
 
     public void OnEnterState()
     {
         //When we enter the state, we find the player's PlayerController and its fsm to send the condition has changed.
+        if (ThePlayer == null)
+        {
+            ThePlayer = GameObject.Find("Player");
+        }
+
         if (MyPlayerController == null)
         {
-            MyPlayerController = GameObject.Find("Player").GetComponent<PlayerController>();
+            MyPlayerController = ThePlayer.GetComponent<PlayerController>();
         }
 
         if (MyFsm == null)
         {
-            MyFsm = GameObject.Find("Player").GetComponent<FSM>();
+            MyFsm = ThePlayer.GetComponent<FSM>();
         }
     }
 
     public void UpdateState()
     {
-        MyPlayerController.Move(InputManager.GetJoystickMovement());
+        Vector3 LastPosition = ThePlayer.transform.position;
+        Vector3 Movement = InputManager.GetJoystickMovement();
+
+        if (Movement != Vector3.zero)
+        {
+            MyPlayerController.Move(Movement);
+            MyPlayerController.SetDirection((ThePlayer.transform.position - LastPosition).normalized);
+        }
     }
 
     public void OnExitState()
     {
-        MyFsm.PopState();
+       MyFsm.PopState();
     }
 }
