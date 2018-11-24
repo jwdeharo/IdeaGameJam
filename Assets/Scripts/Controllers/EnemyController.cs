2052a0e1523f42a69f9063dfc29c961cf1b79574
James <jwdeharo@gmail.com>
2018-11-24 18:18:25 +0100

    AI enemy implementation

---
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    private EnemyIdleState MyIdleState;
+    private FSM MyFsm;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        MyFsm       = GetComponent<FSM>();
+
+        //Init of the enemy states.
+        MyIdleState = new EnemyIdleState();
+
+        MyFsm.AddState("Idle", MyIdleState);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		//In the idle state we wait to start patrolling.
+
+	}
+}
---
+++ Assets/Scripts/FSM/Enemies/EnemyIdleState.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyIdleState : IState
+{
+    public float TimeToWait = 10.0f;
+    private float TimeWaiting;
+
+    public void OnEnterState()
+    {
+        TimeWaiting = TimeToWait;
+    }
+
+
+    public void OnExitState()
+    {
+
+    }
+
+    public void UpdateState()
+    {
+        if (TimeWaiting <= 0.0f)
+        {
+            Debug.Log("TIME TO START PATROLLING");
+            TimeWaiting = TimeToWait;
+        }
+        else
+        {
+            TimeWaiting -= Time.deltaTime;
+        }
+    }
+}
