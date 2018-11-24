2e4651bea7f72af591f6717849913515ecaa4fff
James <jwdeharo@gmail.com>
2018-11-24 09:48:38 +0100

    Creating the dash state

---
+++ Assets/Scripts/FSM/MainCharacter/DashState.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashState : IState {
+
+    private PlayerController MyPlayerController;
+    private FSM MyFsm;
+
+    public void OnEnterState()
+    {
+        //When we enter the state, we find the player's PlayerController and its fsm to send the condition has changed.
+        MyPlayerController = GameObject.Find("Player").GetComponent<PlayerController>();
+        MyFsm = GameObject.Find("Player").GetComponent<FSM>();
+    }
+
+    public void UpdateState()
+    {
+
+    }
+
+    public void OnExitState()
+    {
+    }
+}
