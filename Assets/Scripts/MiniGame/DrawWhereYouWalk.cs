7b2b397edf2943779e20c26f73fd0aaffd132dc7
Cristobal Herrero <tomomajico@gmail.com>
2018-11-24 18:51:14 +0100

    Draw as you move, take a screenshot only of what you have moved, save it

---
+++ Assets/Scripts/MiniGame/DrawWhereYouWalk.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawWhereYouWalk : MonoBehaviour {
+
+    private GameObject inkToDraw;
+
+	// Use this for initialization
+	void Start () {
+        inkToDraw = (GameObject) Resources.Load("DrawingMaterial");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Instantiate(inkToDraw, transform.position, Quaternion.identity);
+	}
+}
---
+++ Assets/Scripts/MiniGame/TimeRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeRenderer : MonoBehaviour {
+
+    private Text text;
+
+    public BreifExistence timer;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = timer.timeRemaining.ToString("0.00"); ;
+    }
+}
