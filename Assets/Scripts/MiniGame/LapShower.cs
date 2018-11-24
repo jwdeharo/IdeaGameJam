d560e5e6c005c8d7b0eec5745154bd0eab2a3050
Cristobal Herrero <tomomajico@gmail.com>
2018-11-24 21:22:03 +0100

    Circuito de teleport con carga

---
+++ Assets/Scripts/MiniGame/LapShower.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapShower : MonoBehaviour {
+
+    public int numeroDeVueltas = 3;
+
+    private Text text;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = Meta.vueltaActual + "/" + numeroDeVueltas;
+
+        if (Meta.vueltaActual > numeroDeVueltas)
+            Victory();
+	}
+
+    private void Victory()
+    {
+        Debug.Log("Victory");
+    }
+}
