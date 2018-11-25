9eafdfe266fd95dc83048fa9a0c8f13d3f8aa587
James <jwdeharo@gmail.com>
2018-11-25 15:03:13 +0100

    Enemy copying the cut state

---
+++ Assets/Scripts/FSM/Enemies/EnemyCutState.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCutState : EnemyBaseState, IState
+{
+    private FSM MyFsm;
+    private CharacterController MyCharacterController;
+    private EnemyController MyController;
+    private Animator MyAnimator;
+
+    private float TimeInCut;
+    private float MaxTimeInCut = 0.8f;
+
+    public void OnEnterState()
+    {
+
+        if (MyFsm == null)
+        {
+            MyFsm = MyGameObject.GetComponent<FSM>();
+        }
+
+        if (MyCharacterController == null)
+        {
+            MyCharacterController = MyGameObject.GetComponent<CharacterController>();
+        }
+
+        if (MyController == null)
+        {
+            MyController = MyGameObject.GetComponent<EnemyController>();
+        }
+
+        if (MyAnimator == null)
+        {
+            MyAnimator = MyGameObject.GetComponent<Animator>();
+        }
+
+       //MyAnimator.SetBool("Is_cutting", true);
+       //AnimatorStateInfo animationState = MyAnimator.GetCurrentAnimatorStateInfo(0);
+       //AnimatorClipInfo[] myAnimatorClip = MyAnimator.GetCurrentAnimatorClipInfo(0);
+        TimeInCut = 0.3f;
+        Debug.Log("CUUUUUT");
+    }
+
+    public void OnExitState()
+    {
+        MyFsm.PopState();
+        //MyAnimator.SetBool("Is_cutting", false);
+    }
+
+    public void UpdateState()
+    {
+        if (MyController.CanCut)
+        {
+            //we hit the player here.
+            Debug.Log("CUT");
+            if (TimeInCut <= 0.0f)
+            {
+                MyFsm.SetFSMCondition("is_cutting", false);
+            }
+            else
+            {
+                TimeInCut -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            MyFsm.SetFSMCondition("is_cutting", false);
+        }
+    }
+}
