9c30f2845f4beff56b238e3027d0e8924091a37b
James <jwdeharo@gmail.com>
2018-11-25 18:35:24 +0100

    This should to the trick

--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -5,4 +5,5 @@ using UnityEngine;
 public static class DataManager
 {
     public static MechanicManager.E_MECHANICS CurrentMechanic = MechanicManager.E_MECHANICS.NONE_MECHANIC;
+    public static bool CanBeDestroyed = false;
 }
