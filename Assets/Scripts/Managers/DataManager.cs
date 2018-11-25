d480c294fbfcc740907571164e6b2bc34b2ee517
James <jwdeharo@gmail.com>
2018-11-25 17:12:08 +0100

    Preparing a data manager

---
+++ Assets/Scripts/Managers/DataManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DataManager
+{
+    public static void GetMechanic(MechanicManager.E_MECHANICS aMechanic)
+    {
+        GameObject Player = GameObject.Find("Player");
+    }
+}
